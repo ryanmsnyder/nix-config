09d57810c218851aa13e7e2e613c7023242878c0
Ryan Snyder <ryansnyder4@gmail.com>
2024-03-06 22:48:17 -0700

    add default.nix to templates dir

---
+++ templates/default.nix
@@ -0,0 +1,10 @@
+{
+  poetry = {
+    path = ./poetry;
+    description = "Python template using poetry with mkShell and venvHook";
+  };
+  pythonVenv = {
+    path = ./pythonVenv;
+    description = "Python development shell using mkShell and venvHook";
+  };
+}
