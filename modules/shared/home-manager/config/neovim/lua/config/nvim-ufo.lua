bc31ae41f3e444729f73b5fd9324d514950f6d04
Ryan Snyder <ryansnyder4@gmail.com>
2024-03-16 09:34:57 -0600

    fix: nvim-ufo config was preventing format on save from working

--- modules/shared/home-manager/config/neovim/lua/config/nvim-ufo.lua
+++ modules/shared/home-manager/config/neovim/lua/config/nvim-ufo.lua
@@ -13,18 +13,18 @@ vim.keymap.set("n", "zK", function()
 	end
 end, { desc = "Peek Fold" })
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-	dynamicRegistration = false,
-	lineFoldingOnly = true,
-}
-local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-for _, ls in ipairs(language_servers) do
-	require("lspconfig")[ls].setup({
-		capabilities = capabilities,
-		-- you can add other fields for setting up lsp server in this table
-	})
-end
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.foldingRange = {
+-- 	dynamicRegistration = false,
+-- 	lineFoldingOnly = true,
+-- }
+-- local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+-- for _, ls in ipairs(language_servers) do
+-- 	require("lspconfig")[ls].setup({
+-- 		capabilities = capabilities,
+-- 		-- you can add other fields for setting up lsp server in this table
+-- 	})
+-- end
 
 require("ufo").setup({
 	provider_selector = function(bufnr, filetype, buftype)
